0587ec3d137ea43198ed1af95f023ec9e27657ea
Yossef Rawy <yossefrwy@gamil.com>
2023-02-18 13:43:47 +0200

    .

--- Profile/Calc-main/calculator/script.js
+++ Profile/Calc-main/calculator/script.js
@@ -1,78 +1,78 @@
 const numberButtons = document.querySelectorAll('[data-number]');
-const operaButtons = document.querySelectorAll("[data-opertaion]");
+const operationButtons = document.querySelectorAll("[data-opertaion]");
 const display = document.querySelector("[data-display]");
 const Operationn = document.querySelectorAll("[data-operation]");
 const allClear = document.querySelector("[data-all-clear]");
 const dataDelete = document.querySelector("[data-delete]");
-const dataEqual = document.querySelector("[data-equals]");
+const operationEquals = document.querySelector("[data-equals]");
 
-let firstNum = '' ;
-let secondNum = '';
+let firstNum = 0
+let secondNum = 0
 let operator = null;
-let result = '';
+let result = 0
 
 
 
+dataDelete.addEventListener('click',() =>{
+    if(display.textContent == operator) operator = null
+    display.textContent = display.textContent.slice(0,-1);
 
-dataEqual.addEventListener('click', () => operate(firstNum,secondNum,operator));
+})
+
+
+
 
 allClear.addEventListener('click', function(){
-  display.textContent = '',
-  firstNum = '',
-  secondNum = '',
-  operator = null,
-  result = '';
+  window.location.reload()
 });
 
-dataDelete.addEventListener("click", clearAll);
-
-
-
-
-
-
-function clearAll(){
-  let String = display.innerText.toString();
-  display.innerText = String.substr(0, String.length -1);
-  firstNum = display.innerText
-  console.log(firstNum)
-  
-}
-
-
-
-function btnPressed(){
-  numberButtons.forEach((button) =>
+numberButtons.forEach((button) =>
     button.addEventListener("click",() =>{
+    if(button.textContent === '.' && display.textContent.includes('.')) return
+    if(display.textContent == '+' ||
+      display.textContent == '-' ||
+      display.textContent == '/' ||
+      display.textContent == '*'){ 
+        return display.textContent = '' }
+    display.textContent += button.textContent
 
-    if(display.textContent === operator) clearAll();
-    if(button.textContent === "." && display.textContent.includes(".")) return;
-    (display.textContent += button.textContent)
-    if (operator == null) {
-      // Read first number if no operator set yet
-      firstNum += button.textContent;
-      console.log("firstnom", firstNum);
-    } else {
-      // Read second number
-      
-      secondNum += button.textContent;
-      console.log("secondnom", secondNum);
-    }
-    }));
     
-    Operationn.forEach((button) =>
-      button.addEventListener("click",() => {
-        if (operator == null){
-          display.textContent = "";
-          display.textContent += button.textContent;
-          operator = button.textContent;
-          console.log(operator);
-        } return;
-        }));
 
-}
+    if(operator != null) {
+      secondNum = display.textContent
+    }else{
+      firstNum = display.textContent
+    }
+    console.log(firstNum)
+    console.log(secondNum)
+    console.log(operator)
+    
+    }))
 
-btnPressed();
+
+
+Operationn.forEach((oButton) => 
+    oButton.addEventListener("click", () => {
+    if(!firstNum) return
+    if(operator != null) return
+    display.textContent = oButton.textContent
+    operator = oButton.textContent
+    }))
+
+
+
+ operationEquals.addEventListener('click', 
+ () => operate(firstNum,secondNum,operator) );
+
+
+
+
+
+
+
+
+  
+
 
 
 
@@ -80,31 +80,43 @@ btnPressed();
 
 
 function operate(firstNum,secondNum,operator){
-    if (operator === '+') return  add(firstNum,secondNum) ;
-    if (operator === '-') return subtract(firstNum,secondNum);
-    if (operator === "*") return multiply(firstNum,secondNum);
-    if (operator === "/") return divide(firstNum,secondNum);
-    return console.log('Enter a Correct Operator');
+    if (operator == '+') return  add(firstNum,secondNum) ;
+    if (operator == '-') return subtract(firstNum,secondNum);
+    if (operator == "*") return multiply(firstNum,secondNum);
+    if (operator == "/") return divide(firstNum,secondNum);
+    console.log('Enter a Correct Operator');
 }
 
 function add(x,y){
   let xx = parseFloat(x)
   let yx = parseFloat(y)
   result = xx + yx;
-  return display.textContent = result;
+  display.textContent = result;
+  returnToNormal()
 
 }
 function subtract(x,y){
   result = x - y;
-  return (display.textContent = result);
+  display.textContent = result;
+  returnToNormal()
 
 }
 function multiply(x,y){
   result = x * y;
-  return (display.textContent = result);
+  display.textContent = result;
+  returnToNormal()
 }
 function divide(x,y){
   result = x / y;
+  display.textContent = result;
+  returnToNormal()
+}
 
-  return (display.textContent = result);
+function returnToNormal(){
+  firstNum = result
+  secondNum = 0
+  operator = null
+  result = 0
+
+  console.log(firstNum,secondNum,operator,result)
 }
