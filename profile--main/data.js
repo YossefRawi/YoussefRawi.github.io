c14c48c5ce9e5fe8710f2558c60032eb446f7502
Yossef Rawy <yossefrwy@gamil.com>
2023-04-15 23:29:35 +0200

    .

--- profile--main/data.js
+++ profile--main/data.js
@@ -1,72 +1,68 @@
 const Data =
 [{
-    id:0,
+    
     name: '<===',
     class: 'go-back',
     link:'',
-},{  id:1,
+},{  
     name:"Tic-Tac-Toe",
-    image:"../public/assets/portoTTT.png",
-    desc:"Simple game of Tic-Tac-Toe using HTML, CSS, JAVASCRIPT.",
-    class:"Tic-Tac-Toe",
     link:'../Profile/Js-Tic-Tac-Toe/TTT.html'
     
 },
-{   id:2,
+{   
     name:"Calculator ",
-    image:"../public/assets/portoCalc.png",
-    desc:"A calculator built using HTML, CSS, JAVASCRIPT.",
-    class:"calculator",
     link:'../Profile/Calc-main/calculator/Index.html'
 },
-{   id:3,
+{   
     name:"To do List",
-    image:"../public/assets/portot.png",
-    desc:"Built with HTML, CSS, JAVASCRIPT and WEBPACK.",
-    class:"Todo-List",
     link:'../Profile/JS_ToDoList/dist/index.html'
 },
 {   id:4,
     name:"Library",
-    image:"../public/assets/portola.png",
-    desc:"A library app using HTML, CSS, JAVASCRIPT.",
     class:"library",
     link:'../Profile/JS-Library-App/library.html'
 },
-{   id:5,
+{   
     name:"Rock Paper Scissors",
-    image:"../public/assets/portoRPS.png",
-    desc:"A game of Rock-Paper-Scissors using HTML, CSS, JAVASCRIPT.",
-    class:"rps",
     link:'../Profile/Rock-Paper-Scissors-main/rps/RockPaperScissors.html'
 },
-{   id:6,
+{  
     name:"Restaurant Page",
-    image:"../public/assets/portorp.png",
-    desc:"A Restaurant Page using HTML, CSS, JAVASCRIPT and WEBPACK.",
-    class:"Restaurant-Page",
     link:'../Profile/Restaurant-Page/dist/index.html'
 },
-{   id:7,
+{   
     name:"Sign Up Form",
-    image:"../public/assets/portoform.png",
-    desc:"A Signup-Form using HTML, CSS, JAVASCRIPT with validation.",
-    class:"signup",
     link:'../Profile/New-Form/Form---main/signup.html'
 },
-{   id:8,
+{  
     name:"Landing Page",
-    image:"../public/assets/protoLP.png",
-    desc:"A Landing-Page using HTML, CSS.",
-    class:"Landing-Page",
     link:'../Profile/Landing-Page/Home/index.html'
 },
-{   id:9,
+{   
     name:"Weather App",
-    image:"../public/assets/protoWa.png",
-    desc:"A Restaurant Page using HTML, CSS, JAVASCRIPT and WEBPACK.",
-    class:"weatherApp",
     link:'../Profile/Weather-App/Weatherapp.html'
+},
+{   
+    name:"REACT Shopping-Cart (REPO)",
+    link:'https://github.com/YossefRawi/react-shopping-cart'
+},
+{   
+    name:"REACT Meme-Generator (REPO)",
+    link:'https://github.com/YossefRawi/meme-generator'
+}
+,
+{   
+    name:"REACT Memory-Game (REPO)",
+    link:'https://github.com/YossefRawi/memory-game'
+},
+{   
+    name:"REACT Tenzies (REPO)",
+    link:'https://github.com/YossefRawi/tenzies'
+}
+,
+{   
+    name:"REACT CV Maker (REPO)",
+    link:'https://github.com/YossefRawi/react-CV-maker'
 }]
 
 
